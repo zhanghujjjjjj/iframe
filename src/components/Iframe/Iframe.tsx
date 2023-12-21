b24f4f432e85e0d850d255490a0d99d3726a0cc1
zhanghujjjjjj <154399008+zhanghujjjjjj@users.noreply.github.com>
2023-12-21 14:11:56 +0800

    Update Iframe.tsx

--- src/components/Iframe/Iframe.tsx
+++ src/components/Iframe/Iframe.tsx
@@ -1,6 +1,6 @@
 
 export function Iframe() {
   return <iframe 
-  style={{ width: '900px', height: '900px', position: 'fixed', top: 0, left: 0 }} 
+  style={{ width: '1900px', height: '900px', position: 'fixed', top: 0, left: 0 }} 
   src="https://0gon984kvrrve.cave.momen.app/"></iframe>;
 }
