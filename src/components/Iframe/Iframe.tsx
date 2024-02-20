2919b3d4ae8d851a7f2c57e11fca0e407b846e16
zhanghujjjjjj <154399008+zhanghujjjjjj@users.noreply.github.com>
2024-02-20 17:27:42 +0800

    2.0.3

--- src/components/Iframe/Iframe.tsx
+++ src/components/Iframe/Iframe.tsx
@@ -2,5 +2,5 @@
 export function Iframe() {
   return <iframe 
   style={{ width: '100%', height: '100%', border: "0",borderRadius: "30px"}} 
-  src="https://0gon984kvrrve.cave.momen.app/"></iframe>;
+  src="https://0gon984kvrrve.villa.momen.app/"></iframe>;
 }
