187084e9975e4d8922046b6e2e2473d0e0c90fff
zhanghujjjjjj <154399008+zhanghujjjjjj@users.noreply.github.com>
2024-11-11 16:57:37 +0800

    Update Iframe.tsx

--- src/components/Iframe/Iframe.tsx
+++ src/components/Iframe/Iframe.tsx
@@ -1,4 +1,4 @@
 
 export function Iframe() {
-  return <a href="https://www.producthunt.com/posts/momen?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-momen" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=591886&theme=light" alt="Momen - Build&#0032;your&#0032;MVP&#0032;and&#0032;scale&#0032;it&#0044;&#0032;no&#0032;code&#0032;needed&#0046; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>;
+  return <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=591886&theme=light" alt="Momen - Build&#0032;your&#0032;MVP&#0032;and&#0032;scale&#0032;it&#0044;&#0032;no&#0032;code&#0032;needed&#0046; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" />;
 }
