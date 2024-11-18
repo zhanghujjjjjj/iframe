864074eed464df99a067a5db503798b52879b629
zhanghujjjjjj <154399008+zhanghujjjjjj@users.noreply.github.com>
2024-11-18 16:48:01 +0800

    Update Iframe.tsx

--- src/components/Iframe/Iframe.tsx
+++ src/components/Iframe/Iframe.tsx
@@ -1,12 +1,12 @@
 export function Iframe() {  
   return (  
     <div style={{ width: '100%', height: '100%', border: "0", borderRadius: "30px" }}>  
-      <a href="https://www.producthunt.com/posts/momen?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-momen">  
+      <a href="https://www.producthunt.com/posts/momen?embed=true&utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-momen">  
         <img  
-          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=591886&theme=dark"  
+          src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=591886&theme=light&period=daily"  
           alt="Momen - Build&#0032;your&#0032;MVP&#0032;and&#0032;scale&#0032;it&#0044;&#0032;no&#0032;code&#0032;needed&#0046; | Product Hunt"  
           width="250px"  
-          height="45px"  
+          height="54px"  
         />  
       </a>  
     </div>  
